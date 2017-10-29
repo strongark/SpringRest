742b50bcd7b4157902b183aa41ab8dd6658efd0e
tranpham <pminhtran@gmail.com>
2017-10-29 15:08:23 -0700

    basic hello world api

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String readme(){
+        return "Repository of RestAPI which utilized Hibernate";
+    }
+}
